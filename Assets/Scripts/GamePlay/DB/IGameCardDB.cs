4aaee556bce80867c20a9f9535eb30834cbf401d
杨循 <929500@nd.com>
2020-11-26 20:34:24 +0800

    使用sqlite做数据存储

---
+++ Assets/Scripts/GamePlay/DB/IGameCardDB.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGameCardDB
+{
+    GameCard GetCard(int id);
+    List<GameCard> GetCards(GameCard.CardType type);
+    List<GameCard> GetCardsWithCost(int cost,GameCard.CardType type);
+    List<GameCard> GetCardsWithCostMax(int cost, GameCard.CardType type);
+    List<GameCard> GetCardsWithCostMin(int cost, GameCard.CardType type);
+}
---
+++ Assets/Scripts/GamePlay/DB/SqliteGameCardDB.cs
@@ -0,0 +1,43 @@
+﻿using SQLite4Unity3d;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SqliteGameCardDB : IGameCardDB
+{
+    private SQLiteConnection _connection;
+
+    public SqliteGameCardDB()
+    {
+        var ta = Resources.Load<TextAsset>("DB/GameCardDB.bytes");
+        var outPath = Path.Combine(Application.persistentDataPath, "DB", "GameCardDB.db");
+        File.WriteAllBytes(outPath, ta.bytes);
+        _connection = new SQLiteConnection(outPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+    }
+
+    public GameCard GetCard(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<GameCard> GetCards(GameCard.CardType type)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<GameCard> GetCardsWithCost(int cost, GameCard.CardType type)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<GameCard> GetCardsWithCostMax(int cost, GameCard.CardType type)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<GameCard> GetCardsWithCostMin(int cost, GameCard.CardType type)
+    {
+        throw new System.NotImplementedException();
+    }
+}
